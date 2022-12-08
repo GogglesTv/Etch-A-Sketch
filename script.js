555e9b1c07d21f834d628a7021db9ac3e80e1950
Jalen Moore <jalenmarquise.moore@gmail.com>
2022-12-08 11:23:50 -0600

    Moved column and square selectors into global scope to use within button event listener

--- script.js
+++ script.js
@@ -2,13 +2,20 @@
 
 const container = document.querySelector("#container");
 const button = document.querySelector("button");
+let column = document.createElement("div");
+let square = document.createElement("div");
 
 for (let i = 1; i <= 16; i++) {
-  let column = document.createElement("div");
+  column = document.createElement("div");
   container.append(column);
   for (let x = 1; x <= 16; x++) {
-    let square = document.createElement("div");
+    square = document.createElement("div");
     square.classList.add("box");
     column.append(square);
   }
 }
+
+button.addEventListener("click", () => {
+  container.remove(column);
+  column.remove(square);
+});
