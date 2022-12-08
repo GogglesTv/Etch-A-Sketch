9a02d47048f8a3da5a0fb07b6fb987a7cb5adc28
Jalen Moore <jalenmarquise.moore@gmail.com>
2022-12-08 11:55:39 -0600

    Copy and pasted some of code from top into resize function. Replaced old variables with new ones.

--- script.js
+++ script.js
@@ -31,4 +31,20 @@ function resize() {
   );
   if (!newGridSize === "number")
     Number(prompt("Invalid input. Enter a number (i.e. 10 = 10x10"));
+
+  const newContainer = document.createElement("div");
+  newContainer.setAttribute("id", "container");
+  let newColumn = document.createElement("div");
+  let newSquare = document.createElement("div");
+
+  for (let i = 1; i <= newGridSize; i++) {
+    section2.append(newContainer);
+    newColumn = document.createElement("div");
+    newContainer.append(newColumn);
+    for (let x = 1; x <= newGridSize; x++) {
+      newSquare = document.createElement("div");
+      newSquare.classList.add("box");
+      newColumn.append(newSquare);
+    }
+  }
 }
