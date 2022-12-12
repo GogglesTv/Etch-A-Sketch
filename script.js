b1f0ebb4228da3a8f3ab78f28c1cb998eed21cf6
Jalen Moore <jalenmarquise.moore@gmail.com>
2022-12-12 12:12:46 -0600

    Found a much better way to make grid and found solution to keep grid within container.

--- script.js
+++ script.js
@@ -1,81 +1,62 @@
 "use strict";
 
-const section2 = document.querySelector(".section2");
-const container = document.querySelector("#container");
-const box = document.querySelectorAll(".box");
 const resetButton = document.querySelector("#reset");
-const gridItem = document.querySelectorAll("div#square");
+const clearButton = document.querySelector("#clear");
 
-// let column = document.createElement("div");
-// let square = document.createElement("div");
+let cell = document.createElement("div");
+let container = document.querySelector("#container");
 
-// for (let i = 1; i <= 16; i++) {
-//   column = document.createElement("div");
-//   column.classList.add("column");
-//   container.append(column);
-//   for (let x = 1; x <= 16; x++) {
-//     square = document.createElement("div");
-//     square.classList.add("box");
-//     square.id = "square";
-//     column.append(square);
-//   }
-// }
-
-// resetButton.addEventListener("click", reset);
-
-// function reset() {
-//   container.remove(column);
-//   column.remove(square);
-
-//   let newGridSize = Number(
-//     prompt("Choose a number from 1 - 100 to resize the grid. (i.e. 10 = 10x10")
-//   );
-//   if (!newGridSize === "number")
-//     Number(prompt("Invalid input. Enter a number (i.e. 10 = 10x10"));
-
-//   container = document.createElement("div");
-//   container.setAttribute("id", "container");
-//   column = document.createElement("div");
-//   square = document.createElement("div");
-
-//   for (let i = 1; i <= newGridSize ** 2; i++) {
-//     section2.append(container);
-//     column = document.createElement("div");
-//     column.classList.add("column");
-//     container.append(column);
-//     for (let x = 1; x <= newGridSize; x++) {
-//       square = document.createElement("div");
-//       square.classList.add("box");
-//       square.id = "square";
-//       column.append(square);
-//     }
-//   }
-// }
-// gridItem.addEventListener("mouseenter", (event) => {
-//   console.log(event.target);
-//   event.target.style.backgroundColor = "black";
-// });
-
-const grid = { rows: 16, cols: 16 };
-const total = grid.rows * grid.cols;
-const container1 = document.createElement("div");
-document.body;
-
-createGrid(total);
-
-function createGrid(total) {
-  for (let i = 1; i <= total; i++) {
-    const ele = document.createElement("div");
-    container.append(ele);
-    ele.textContent = `${i}`;
-    ele.classList.add("box");
-    ele.style.backgroundColor = "white";
-    ele.addEventListener("mouseenter", (e) => {
+createGrid();
+function createGrid() {
+  for (let i = 1; i <= 256; i++) {
+    cell = document.createElement("div");
+    container.append(cell);
+    cell.classList.add("box");
+    cell.style.backgroundColor = "white";
+    cell.addEventListener("mouseenter", (e) => {
       e.target.style.backgroundColor = "black";
     });
   }
+
+  container.style.setProperty(`grid-template-columns`, `repeat(16, 2fr)`);
+  container.style.setProperty(`grid-template-rows`, `repeat(16, 2fr)`);
+}
+
+resetButton.addEventListener("click", reset);
+function reset() {
+  container.innerHTML = "";
+
+  let newGridSize = Number(
+    prompt("Choose a number from 1 - 100 to resize the grid. (i.e. 10 = 10x10")
+  );
+  if (newGridSize > 100 || newGridSize < 1 || newGridSize === NaN) {
+    Number(prompt("Invalid input. Enter a number (i.e. 10 = 10x10"));
+  } else createGrid();
+
   container.style.setProperty(
     `grid-template-columns`,
-    `repeat(${grid.cols}, 1fr)`
+    `repeat(${newGridSize}, 2fr)`
   );
+  container.style.setProperty(
+    `grid-template-rows`,
+    `repeat(${newGridSize}, 2fr)`
+  );
+
+  for (let i = 1; i <= newGridSize ** 2; i++) {
+    cell = document.createElement("div");
+    container.append(cell);
+    cell.classList.add("box");
+    cell.style.backgroundColor = "white";
+    cell.addEventListener("mouseenter", (e) => {
+      e.target.style.backgroundColor = "black";
+    });
+  }
+}
+
+clearButton.addEventListener("click", clear);
+function clear() {
+  let gridBox = document.querySelectorAll(".box");
+  for (const box of gridBox) {
+    box.style.backgroundColor = "white";
+  }
 }
